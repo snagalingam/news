5070e8f4169801d76c8717371414ca86d231c419
Sinthuja Nagalingam <sinthu.n@gmail.com>
2020-02-25 13:46:11 -0600

    adds the framework for the news page

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ require "geocoder"
 require "forecast_io"
 require "httparty"
 def view(template); erb template.to_sym; end
-before { puts "Parameters: #{params}" }                                     
+before { puts "Parameters: #{params}" }
 
 # enter your Dark Sky API key here
 ForecastIO.api_key = "YOUR-API-KEY"
 
 get "/" do
-  # show a view that asks for the location
+  view "ask"
 end
 
 get "/news" do
-  # do everything else
+  view "news"
 end
